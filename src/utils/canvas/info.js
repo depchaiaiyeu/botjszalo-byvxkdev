db234e1c62fed0420cc0b4b6301ca3d00e9bd9e2
depchaiaiyeu <kiennone@gmail.com>
2025-10-26 18:28:21 +0700

    Update info.js

--- src/utils/canvas/info.js
+++ src/utils/canvas/info.js
@@ -4,6 +4,22 @@ import path from "path";
 import * as cv from "./index.js";
 import { formatCurrency } from "../format-util.js";
 
+function roundRect(ctx, x, y, w, h, r, fill = false, stroke = false) {
+  ctx.beginPath();
+  ctx.moveTo(x + r, y);
+  ctx.lineTo(x + w - r, y);
+  ctx.quadraticCurveTo(x + w, y, x + w, y + r);
+  ctx.lineTo(x + w, y + h - r);
+  ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+  ctx.lineTo(x + r, y + h);
+  ctx.quadraticCurveTo(x, y + h, x, y + h - r);
+  ctx.lineTo(x, y + r);
+  ctx.quadraticCurveTo(x, y, x + r, y);
+  ctx.closePath();
+  if (fill) ctx.fill();
+  if (stroke) ctx.stroke();
+}
+
 export function hanldeNameUser(name) {
   const words = name.split(" ");
   let line1 = "";
