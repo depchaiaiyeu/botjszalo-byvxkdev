77cd97db59a473660cca7965a8c1ef04553d91dc
depchaiaiyeu <kiennone@gmail.com>
2025-10-28 18:23:25 +0700

    Update lich-van-nien.js

--- src/commands/utilities-command/lich-van-nien.js
+++ src/commands/utilities-command/lich-van-nien.js
@@ -1,17 +1,9 @@
 import { sendMessageQuery } from "../../service-hahuyhoang/chat-zalo/chat-style/chat-style.js";
-import { getGlobalPrefix } from "../../service-hahuyhoang/service.js";
-import { removeMention } from "../../utils/format-util.js";
 import fs from "fs/promises";
 import { createCalendarImage, clearImagePath } from "../../utils/canvas/calendar.js";
 
 export async function handleCalendarCommand(api, message) {
-  const content = removeMention(message);
-  const prefix = getGlobalPrefix();
   const threadId = message.threadId;
-
-  if (!content.includes(`${prefix}lichvannien`)) {
-    return false;
-  }
 
   try {
     const imagePath = await createCalendarImage();
@@ -19,7 +11,7 @@ export async function handleCalendarCommand(api, message) {
     await api.sendMessage(
       {
         msg: "📅 Lịch Vạn Niên",
-        attachments: [imagePath]
+        attachments: [imagePath],
       },
       threadId,
       message.type
@@ -28,7 +20,11 @@ export async function handleCalendarCommand(api, message) {
     await clearImagePath(imagePath);
   } catch (error) {
     console.error("Lỗi khi tạo lịch vạn niên:", error);
-    await sendMessageQuery(api, message, "Đã xảy ra lỗi khi tạo lịch vạn niên. Vui lòng thử lại sau.");
+    await sendMessageQuery(
+      api,
+      message,
+      "Đã xảy ra lỗi khi tạo lịch vạn niên. Vui lòng thử lại sau."
+    );
   }
 
   return true;
