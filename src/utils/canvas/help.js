009e83ae5bd0baf2238dddb3d5b74825591fbd55
depchaiaiyeu <kiennone@gmail.com>
2025-11-07 17:35:51 +0700

    Update help.js

--- src/utils/canvas/help.js
+++ src/utils/canvas/help.js
@@ -53,149 +53,94 @@ export function createHelpBackground(ctx, width, height) {
   ctx.shadowBlur = 0;
 }
 
-function toUpperCaseVietnamese(text) {
-  return text.replace(/[a-zA-Zàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđ]/g, (char) => {
-    const lowerToUpper = {
-      'à': 'À', 'á': 'Á', 'ạ': 'Ạ', 'ả': 'Ả', 'ã': 'Ã',
-      'â': 'Â', 'ầ': 'Ầ', 'ấ': 'Ấ', 'ậ': 'Ậ', 'ẩ': 'Ẩ', 'ẫ': 'Ẫ',
-      'ă': 'Ă', 'ằ': 'Ằ', 'ắ': 'Ắ', 'ặ': 'Ặ', 'ẳ': 'Ẳ', 'ẵ': 'Ẵ',
-      'è': 'È', 'é': 'É', 'ẹ': 'Ẹ', 'ẻ': 'Ẻ', 'ẽ': 'Ẽ',
-      'ê': 'Ê', 'ề': 'Ề', 'ế': 'Ế', 'ệ': 'Ệ', 'ể': 'Ể', 'ễ': 'Ễ',
-      'ì': 'Ì', 'í': 'Í', 'ị': 'Ị', 'ỉ': 'Ỉ', 'ĩ': 'Ĩ',
-      'ò': 'Ò', 'ó': 'Ó', 'ọ': 'Ọ', 'ỏ': 'Ỏ', 'õ': 'Õ',
-      'ô': 'Ô', 'ồ': 'Ồ', 'ố': 'Ố', 'ộ': 'Ộ', 'ổ': 'Ổ', 'ỗ': 'Ỗ',
-      'ơ': 'Ơ', 'ờ': 'Ờ', 'ớ': 'Ớ', 'ợ': 'Ợ', 'ở': 'Ở', 'ỡ': 'Ỡ',
-      'ù': 'Ù', 'ú': 'Ú', 'ụ': 'Ụ', 'ủ': 'Ủ', 'ũ': 'Ũ',
-      'ư': 'Ư', 'ừ': 'Ừ', 'ứ': 'Ứ', 'ự': 'Ự', 'ử': 'Ử', 'ữ': 'Ữ',
-      'ỳ': 'Ỳ', 'ý': 'Ý', 'ỵ': 'Ỵ', 'ỷ': 'Ỷ', 'ỹ': 'Ỹ',
-      'đ': 'Đ'
-    };
-    return lowerToUpper[char] || char.toUpperCase();
-  });
-}
-
-function roundRect(ctx, x, y, width, height, radius) {
-  ctx.beginPath();
-  ctx.moveTo(x + radius, y);
-  ctx.lineTo(x + width - radius, y);
-  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-  ctx.lineTo(x + width, y + height - radius);
-  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-  ctx.lineTo(x + radius, y + height);
-  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-  ctx.lineTo(x, y + radius);
-  ctx.quadraticCurveTo(x, y, x + radius, y);
-  ctx.closePath();
-}
-
-export async function createInstructionsImage(helpContent, isAdminBox, width = 880) {
-  const padding = 40;
-  const boxPadding = 16;
-  const boxGap = 15;
-  const borderRadius = 12;
-  const titleHeight = 90;
-  const boxHeight = 60;
-  const rowMargin = 15;
-  
-  const allCommands = [];
+export async function createInstructionsImage(helpContent, isAdminBox, width = 800) {
+  const ctxTemp = createCanvas(999, 999).getContext("2d");
+  const space = 36;
+  let yTemp = 60;
+  ctxTemp.font = "bold 28px BeVietnamPro";
   for (const key in helpContent.allMembers) {
     if (helpContent.allMembers.hasOwnProperty(key)) {
-      allCommands.push(helpContent.allMembers[key]);
+      const keyHelpContent = `${helpContent.allMembers[key].icon} ${helpContent.allMembers[key].command}`;
+      const labelWidth = ctxTemp.measureText(keyHelpContent).width;
+      const valueHelpContent = " -> " + helpContent.allMembers[key].description;
+      const lineWidth = labelWidth + space + ctxTemp.measureText(valueHelpContent).width;
+      if (lineWidth > width) yTemp += 52;
+      yTemp += 52;
     }
   }
-  
-  const adminCommands = [];
+  yTemp += 60;
   if (isAdminBox) {
     for (const key in helpContent.admin) {
       if (helpContent.admin.hasOwnProperty(key)) {
-        adminCommands.push(helpContent.admin[key]);
+        const keyHelpContent = `${helpContent.admin[key].icon} ${helpContent.admin[key].command}`;
+        const labelWidth = ctxTemp.measureText(keyHelpContent).width;
+        const valueHelpContent = " -> " + helpContent.admin[key].description;
+        const lineWidth = labelWidth + space + ctxTemp.measureText(valueHelpContent).width;
+        if (lineWidth > width) yTemp += 52;
+        yTemp += 52;
+      }
+    }
+    yTemp += 60;
+  }
+  const height = yTemp > 430 ? yTemp : 430;
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext("2d");
+  createHelpBackground(ctx, width, height);
+  let y = 60;
+  ctx.textAlign = "left";
+  ctx.font = "bold 28px BeVietnamPro";
+  ctx.fillStyle = cv.getRandomGradient(ctx, width);
+  ctx.fillText(helpContent.title, space, y);
+  y += 50;
+  ctx.textAlign = "left";
+  ctx.font = "bold 28px BeVietnamPro";
+  ctx.fillStyle = "#FFFFFF";
+  for (const key in helpContent.allMembers) {
+    if (helpContent.allMembers.hasOwnProperty(key)) {
+      ctx.fillStyle = cv.getRandomGradient(ctx, width);
+      const keyHelpContent = `${helpContent.allMembers[key].icon} ${helpContent.allMembers[key].command}`;
+      const labelWidth = ctx.measureText(keyHelpContent).width;
+      ctx.fillText(keyHelpContent, space, y);
+      ctx.fillStyle = "#FFFFFF";
+      const valueHelpContent = " -> " + helpContent.allMembers[key].description;
+      const lineWidth = labelWidth + space + ctx.measureText(valueHelpContent).width;
+      if (lineWidth > width) {
+        y += 52;
+        ctx.fillText(valueHelpContent, space + 20, y);
+      } else {
+        ctx.fillText(valueHelpContent, space + labelWidth, y);
+      }
+      y += 52;
+    }
+  }
+  if (isAdminBox) {
+    if (Object.keys(helpContent.admin).length > 0) {
+      y += 30;
+      ctx.textAlign = "left";
+      ctx.font = "bold 28px BeVietnamPro";
+      ctx.fillStyle = cv.getRandomGradient(ctx, width);
+      ctx.fillText(helpContent.titleAdmin, space, y);
+      y += 50;
+      for (const key in helpContent.admin) {
+        if (helpContent.admin.hasOwnProperty(key)) {
+          ctx.fillStyle = cv.getRandomGradient(ctx, width);
+          const keyHelpContent = `${helpContent.admin[key].icon} ${helpContent.admin[key].command}`;
+          const labelWidth = ctx.measureText(keyHelpContent).width;
+          ctx.fillText(keyHelpContent, space, y);
+          ctx.fillStyle = "#FFFFFF";
+          const valueHelpContent = " -> " + helpContent.admin[key].description;
+          const lineWidth = labelWidth + space + ctx.measureText(valueHelpContent).width;
+          if (lineWidth > width) {
+            y += 52;
+            ctx.fillText(valueHelpContent, space + 20, y);
+          } else {
+            ctx.fillText(valueHelpContent, space + labelWidth, y);
+          }
+          y += 52;
+        }
       }
     }
   }
-  
-  const leftBoxWidth = 350;
-  const rightBoxWidth = width - padding * 2 - leftBoxWidth - boxGap;
-  
-  let totalHeight = titleHeight + 50;
-  totalHeight += allCommands.length * (boxHeight + rowMargin);
-  
-  if (adminCommands.length > 0) {
-    totalHeight += 90;
-    totalHeight += adminCommands.length * (boxHeight + rowMargin);
-  }
-  
-  totalHeight += padding;
-  
-  const height = Math.max(totalHeight, 430);
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext("2d");
-  
-  createHelpBackground(ctx, width, height);
-  
-  const titleText = toUpperCaseVietnamese(helpContent.title);
-  ctx.font = "bold 30px BeVietnamPro";
-  ctx.textAlign = "center";
-  ctx.fillStyle = cv.getRandomGradient(ctx, width);
-  ctx.fillText(titleText, width / 2, 55);
-  
-  const drawCommandRow = (command, description, y) => {
-    roundRect(ctx, padding, y, leftBoxWidth, boxHeight, borderRadius);
-    ctx.fillStyle = "rgba(26, 58, 80, 0.7)";
-    ctx.fill();
-    ctx.strokeStyle = "rgba(100, 150, 200, 0.4)";
-    ctx.lineWidth = 1.5;
-    ctx.stroke();
-    
-    ctx.textAlign = "left";
-    ctx.font = "bold 19px BeVietnamPro";
-    ctx.fillStyle = cv.getRandomGradient(ctx, leftBoxWidth);
-    ctx.fillText(command, padding + boxPadding, y + boxHeight / 2 + 7);
-    
-    roundRect(ctx, padding + leftBoxWidth + boxGap, y, rightBoxWidth, boxHeight, borderRadius);
-    ctx.fillStyle = "rgba(18, 45, 60, 0.8)";
-    ctx.fill();
-    ctx.strokeStyle = "rgba(100, 150, 200, 0.4)";
-    ctx.lineWidth = 1.5;
-    ctx.stroke();
-    
-    ctx.textAlign = "left";
-    ctx.font = "18px BeVietnamPro";
-    ctx.fillStyle = "#E8E8E8";
-    ctx.fillText(description, padding + leftBoxWidth + boxGap + boxPadding, y + boxHeight / 2 + 7);
-  };
-  
-  let currentY = titleHeight + 50;
-  
-  for (let i = 0; i < allCommands.length; i++) {
-    const cmd = allCommands[i];
-    const commandText = `${cmd.icon} ${cmd.command}`;
-    const descriptionText = cmd.description;
-    
-    drawCommandRow(commandText, descriptionText, currentY);
-    currentY += boxHeight + rowMargin;
-  }
-  
-  if (adminCommands.length > 0) {
-    currentY += 40;
-    
-    const adminTitleText = toUpperCaseVietnamese(helpContent.titleAdmin);
-    ctx.font = "bold 27px BeVietnamPro";
-    ctx.textAlign = "center";
-    ctx.fillStyle = cv.getRandomGradient(ctx, width);
-    ctx.fillText(adminTitleText, width / 2, currentY);
-    
-    currentY += 50;
-    
-    for (let i = 0; i < adminCommands.length; i++) {
-      const cmd = adminCommands[i];
-      const commandText = `${cmd.icon} ${cmd.command}`;
-      const descriptionText = cmd.description;
-      
-      drawCommandRow(commandText, descriptionText, currentY);
-      currentY += boxHeight + rowMargin;
-    }
-  }
-  
   const filePath = path.resolve(`./assets/temp/help_${Date.now()}.png`);
   const out = fs.createWriteStream(filePath);
   const stream = canvas.createPNGStream();
