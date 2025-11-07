8c06ed6232da9d991ca71ccd6969dd205d095b6d
depchaiaiyeu <kiennone@gmail.com>
2025-11-07 12:29:36 +0700

    Update create-webp.js

--- src/service-hahuyhoang/chat-zalo/chat-special/send-sticker/create-webp.js
+++ src/service-hahuyhoang/chat-zalo/chat-special/send-sticker/create-webp.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export async function createCircleWebp(api, message, imageUrl, idImage, rate = null) {
-    const frameRate = rate || 60;
+    const frameRate = rate || 30;
     const ext = await checkExstentionFileRemote(imageUrl);
     const downloadedImage = path.join(tempDir, `original_${idImage}.${ext}`);
     const framesDir = path.join(tempDir, `frames_${idImage}`);
@@ -24,7 +24,7 @@ export async function createCircleWebp(api, message, imageUrl, idImage, rate = n
         await downloadFileFake(imageUrl, downloadedImage);
         const size = 512;
         const borderWidth = 8;
-        const totalFrames = 30;
+        const totalFrames = 60;
         const numWorkers = Math.min(os.cpus().length, totalFrames);
         const framesPerWorker = Math.ceil(totalFrames / numWorkers);
         const resizedImageBuffer = await sharp(downloadedImage)
@@ -98,7 +98,7 @@ export async function createCircleWebp(api, message, imageUrl, idImage, rate = n
     }
 }
 
-export async function convertToWebpMulti(inputPath, outputPath, frameRate = 60) {
+export async function convertToWebpMulti(inputPath, outputPath, frameRate = 30) {
     return new Promise((resolve, reject) => {
         ffmpeg(inputPath)
             .inputOptions([
