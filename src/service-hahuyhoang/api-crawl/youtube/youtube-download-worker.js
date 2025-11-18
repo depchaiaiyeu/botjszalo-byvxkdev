bfbf55f789555af6e09da3655573253a1347dd62
depchaiaiyeu <kiennone@gmail.com>
2025-11-18 10:00:05 +0700

    Update youtube-download-worker.js

--- src/service-hahuyhoang/api-crawl/youtube/youtube-download-worker.js
+++ src/service-hahuyhoang/api-crawl/youtube/youtube-download-worker.js
@@ -1,44 +1,22 @@
-import { parentPort, workerData } from 'worker_threads';
-import youtubedl from 'youtube-dl-exec';
+const { workerData, parentPort } = require('worker_threads');
+const youtubedl = require('youtube-dl-exec');
 
-async function downloadVideo(videoUrl, videoPath, format) {
+const { videoUrl, videoPath, format } = workerData;
+
+(async () => {
   try {
-    let options;
-    if (format === 'bestaudio[ext=aac]/bestaudio[ext=m4a]/bestaudio') {
-      options = {
-        output: videoPath,
-        format: format,
-        noCheckCertificates: true,
-        noWarnings: true,
-        preferFreeFormats: true,
-        bufferSize: "16K",
-        addHeader: ["referer:youtube.com"],
-      };
-    } else {
-      options = {
-        output: videoPath,
-        format: format,
-        mergeOutputFormat: "mp4", 
-        noCheckCertificates: true,
-        noWarnings: true,
-        preferFreeFormats: true,
-        bufferSize: "16K",
-        addHeader: [
-          "referer:youtube.com",
-          "user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        ],
-      };
-    }
-
-    await youtubedl(videoUrl, options);
-    parentPort.postMessage({ success: true, videoPath });
-    
-  } catch (error) {
-    parentPort.postMessage({ 
-      success: false, 
-      error: error.message 
+    await youtubedl(videoUrl, {
+      format: format,
+      output: videoPath,
+      noWarnings: true,
+      noCallHome: true,
+      noCheckCertificate: true,
+      preferFreeFormats: true,
+      youtubeSkipDashManifest: true,
+      addHeader: ['referer:youtube.com', 'user-agent:googlebot']
     });
+    parentPort.postMessage({ success: true, videoPath });
+  } catch (error) {
+    parentPort.postMessage({ success: false, error: error.message });
   }
-}
-
-downloadVideo(workerData.videoUrl, workerData.videoPath, workerData.format); 
+})();
