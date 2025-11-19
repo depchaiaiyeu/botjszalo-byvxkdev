db2d0323609788382767127aa2caafb11e720532
depchaiaiyeu <kiennone@gmail.com>
2025-11-19 11:25:33 +0700

    Update acceptFriendRequest.js

--- src/api-zalo/apis/acceptFriendRequest.js
+++ src/api-zalo/apis/acceptFriendRequest.js
@@ -1,39 +1,42 @@
 import { Zalo, ZaloApiError } from "../index.js";
 import { appContext } from "../context.js";
-import { makeURL, encodeAES, request, handleZaloResponse } from "../utils.js";
+import { encodeAES, handleZaloResponse, request, makeURL } from "../utils.js";
 
 export function acceptFriendRequestFactory(api) {
-    const serviceURL = makeURL(`${api.zpwServiceMap.friend[0]}/api/friend/accept`, {
-        zpw_ver: Zalo.API_VERSION,
-        zpw_type: Zalo.API_TYPE,
+  const serviceURL = makeURL(`${api.zpwServiceMap.friend[0]}/api/friend/accept`, {
+    zpw_ver: Zalo.API_VERSION,
+    zpw_type: Zalo.API_TYPE,
+  });
+
+  /**
+   * Accept a friend request from a User
+   *
+   * @param friendId The friend ID to user request is accept
+   *
+   * @throws {ZaloApiError}
+   */
+  return async function acceptFriendRequest(friendId) {
+    if (!appContext.secretKey || !appContext.imei || !appContext.cookie || !appContext.userAgent)
+      throw new ZaloApiError("Missing required app context fields");
+
+    const params = {
+      fid: friendId,
+      language: appContext.language,
+    };
+
+    const encryptedParams = encodeAES(appContext.secretKey, JSON.stringify(params));
+    if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
+
+    const response = await request(serviceURL, {
+      method: "POST",
+      body: new URLSearchParams({
+        params: encryptedParams,
+      }),
     });
 
-    /**
-     * Accept a friend request from a User
-     *
-     * @param friendId The friend ID to user request is accept
-     *
-     * @throws {ZaloApiError}
-     */
-    return async function acceptFriendRequest(friendId) {
-        const params = {
-            fid: friendId,
-            language: appContext.language,
-        };
+    const result = await handleZaloResponse(response);
+    if (result.error) throw new ZaloApiError(result.error.message, result.error.code);
 
-        const encryptedParams = encodeAES(appContext.secretKey, JSON.stringify(params));
-        if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
-
-        const response = await request(serviceURL, {
-            method: "POST",
-            body: new URLSearchParams({
-                params: encryptedParams,
-            }),
-        });
-
-        const result = await handleZaloResponse(response);
-        if (result.error) throw new ZaloApiError(result.error.message, result.error.code);
-
-        return result.data;
-    };
+    return result.data;
+  };
 }
