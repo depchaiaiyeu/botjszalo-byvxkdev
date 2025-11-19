c9a6f4a8b1c4bc99fcb70b04dd4188fd3bedc028
depchaiaiyeu <kiennone@gmail.com>
2025-11-19 11:26:51 +0700

    Update removeGroupBlockedMember.js

--- src/api-zalo/apis/removeGroupBlockedMember.js
+++ src/api-zalo/apis/removeGroupBlockedMember.js
@@ -1,37 +1,44 @@
 import { Zalo, ZaloApiError } from "../index.js";
 import { appContext } from "../context.js";
-import { makeURL, encodeAES, request, handleZaloResponse } from "../utils.js";
+import { encodeAES, handleZaloResponse, request, makeURL } from "../utils.js";
 
 export function removeGroupBlockedMemberFactory(api) {
-    // THÊM params zpw_ver và zpw_type
-    const serviceURL = makeURL(`${api.zpwServiceMap.group[0]}/api/group/blockedmems/remove`, {
-        zpw_ver: Zalo.API_VERSION,
-        zpw_type: Zalo.API_TYPE,
+  const serviceURL = makeURL(`${api.zpwServiceMap.group[0]}/api/group/blockedmems/remove`, {
+    zpw_ver: Zalo.API_VERSION,
+    zpw_type: Zalo.API_TYPE,
+  });
+
+  /**
+   * Remove group blocked member
+   *
+   * @param memberId member id(s)
+   * @param groupId group id
+   *
+   * @throws {ZaloApiError}
+   */
+  return async function removeGroupBlockedMember(memberId, groupId) {
+    if (!appContext.secretKey || !appContext.imei || !appContext.cookie || !appContext.userAgent)
+      throw new ZaloApiError("Missing required app context fields");
+
+    if (!Array.isArray(memberId)) memberId = [memberId];
+
+    const params = {
+      grid: groupId,
+      members: memberId,
+    };
+
+    const encryptedParams = encodeAES(appContext.secretKey, JSON.stringify(params));
+    if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
+
+    const response = await request(makeURL(serviceURL, {
+      params: encryptedParams,
+    }), {
+      method: "GET",
     });
 
-    /**
-     * Remove group blocked member
-     * @throws {ZaloApiError}
-     */
-    return async function removeGroupBlockedMember(memberId, groupId) {
-        if (!Array.isArray(memberId)) memberId = [memberId];
+    const result = await handleZaloResponse(response);
+    if (result.error) throw new ZaloApiError(result.error.message, result.error.code);
 
-        const params = {
-            grid: String(groupId),
-            members: memberId,
-            imei: appContext.imei,
-        };
-
-        const encryptedParams = encodeAES(appContext.secretKey, JSON.stringify(params));
-        if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
-
-        const response = await request(makeURL(serviceURL, { params: encryptedParams }), {
-            method: "GET",
-        });
-
-        const result = await handleZaloResponse(response);
-        if (result.error) throw new ZaloApiError(result.error.message, result.error.code);
-
-        return result.data;
-    };
+    return result.data;
+  };
 }
