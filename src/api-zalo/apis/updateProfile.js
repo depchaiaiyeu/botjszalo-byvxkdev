de38b14e6d4b0be8ebcd3314d04dcc9b9c9274fc
depchaiaiyeu <kiennone@gmail.com>
2025-11-19 20:15:25 +0700

    Update updateProfile.js

--- src/api-zalo/apis/updateProfile.js
+++ src/api-zalo/apis/updateProfile.js
@@ -1,48 +1,56 @@
-import { Zalo, ZaloApiError } from "../index.js"; // Import Zalo để lấy API_VERSION
+import { Zalo, ZaloApiError } from "../index.js";
 import { appContext } from "../context.js";
-import { makeURL, encodeAES, request, handleZaloResponse } from "../utils.js";
+import { encodeAES, handleZaloResponse, request, makeURL } from "../utils.js";
 
 export function updateProfileFactory(api) {
-    // THÊM params zpw_ver và zpw_type
-    const serviceURL = makeURL(`${api.zpwServiceMap.profile[0]}/api/social/profile/update`, {
-        zpw_ver: Zalo.API_VERSION,
-        zpw_type: Zalo.API_TYPE,
+  const serviceURL = makeURL(`${api.zpwServiceMap.profile[0]}/api/social/profile/update`, {
+    zpw_ver: Zalo.API_VERSION,
+    zpw_type: Zalo.API_TYPE,
+  });
+
+  /**
+   * Change account setting information
+   *
+   * @param payload payload
+   *
+   * @note If your account is a Business Account, include the biz.cate field; otherwise the category will be removed.
+   * You may leave the other biz fields empty if you don’t want to change them.
+   *
+   * @throws {ZaloApiError}
+   */
+  return async function updateProfile(payload) {
+    if (!appContext.secretKey || !appContext.imei || !appContext.cookie || !appContext.userAgent)
+      throw new ZaloApiError("Missing required app context fields");
+
+    const params = {
+      profile: JSON.stringify({
+        name: payload.profile.name,
+        dob: payload.profile.dob,
+        gender: payload.profile.gender,
+      }),
+      biz: JSON.stringify({
+        desc: payload.biz?.description,
+        cate: payload.biz?.cate,
+        addr: payload.biz?.address,
+        website: payload.biz?.website,
+        email: payload.biz?.email,
+      }),
+      language: appContext.language,
+    };
+
+    const encryptedParams = encodeAES(appContext.secretKey, JSON.stringify(params));
+    if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
+
+    const response = await request(serviceURL, {
+      method: "POST",
+      body: new URLSearchParams({
+        params: encryptedParams,
+      }),
     });
 
-    /**
-     * Change account setting information
-     * @throws {ZaloApiError}
-     */
-    return async function updateProfile(payload) {
-        const params = {
-            profile: JSON.stringify({
-                name: payload.profile.name,
-                dob: payload.profile.dob,
-                gender: payload.profile.gender,
-            }),
-            biz: JSON.stringify({
-                desc: payload.biz?.description,
-                cate: payload.biz?.cate,
-                addr: payload.biz?.address,
-                website: payload.biz?.website,
-                email: payload.biz?.email,
-            }),
-            language: appContext.language,
-        };
+    const result = await handleZaloResponse(response);
+    if (result.error) throw new ZaloApiError(result.error.message, result.error.code);
 
-        const encryptedParams = encodeAES(appContext.secretKey, JSON.stringify(params));
-        if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
-
-        const response = await request(serviceURL, {
-            method: "POST",
-            body: new URLSearchParams({
-                params: encryptedParams,
-            }),
-        });
-
-        const result = await handleZaloResponse(response);
-        if (result.error) throw new ZaloApiError(result.error.message, result.error.code);
-
-        return result.data;
-    };
+    return result.data;
+  };
 }
