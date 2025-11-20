08567252365c6ed790a256c1fcacd00088ab98bd
depchaiaiyeu <kiennone@gmail.com>
2025-11-20 16:20:14 +0700

    Create blockUser.js

---
+++ src/api-zalo/apis/blockUser.js
@@ -0,0 +1,42 @@
+import { Zalo, ZaloApiError } from "../index.js";
+import { appContext } from "../context.js";
+import { encodeAES, handleZaloResponse, request, makeURL } from "../utils.js";
+
+export function blockUserFactory(api) {
+  const serviceURL = makeURL(`${api.zpwServiceMap.friend[0]}/api/friend/block`, {
+    zpw_ver: Zalo.API_VERSION,
+    zpw_type: Zalo.API_TYPE,
+  });
+
+  /**
+   * Block a User
+   *
+   * @param userId The ID of the User to block
+   *
+   * @throws {ZaloApiError}
+   */
+  return async function blockUser(userId) {
+    if (!appContext.secretKey || !appContext.imei || !appContext.cookie || !appContext.userAgent)
+      throw new ZaloApiError("Missing required app context fields");
+
+    const params = {
+      fid: userId,
+      imei: appContext.imei,
+    };
+
+    const encryptedParams = encodeAES(appContext.secretKey, JSON.stringify(params));
+    if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
+
+    const response = await request(serviceURL, {
+      method: "POST",
+      body: new URLSearchParams({
+        params: encryptedParams,
+      }),
+    });
+
+    const result = await handleZaloResponse(response);
+    if (result.error) throw new ZaloApiError(result.error.message, result.error.code);
+
+    return result.data;
+  };
+}
